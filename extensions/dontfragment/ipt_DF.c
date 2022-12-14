4077bba62d48dd82ae190e0e2430c92af59ad02e
Florin Popescu <fpopescu@drivenets.com>
2022-12-14 09:12:21 +0200

    replaced skb_make_writable with skb_ensure_writable

--- extensions/dontfragment/ipt_DF.c
+++ extensions/dontfragment/ipt_DF.c
@@ -22,8 +22,8 @@ static unsigned int df_tg(struct sk_buff *skb, const struct xt_action_param *par
 	df_mode mode = ((struct xt_df_tginfo *)(param->targinfo))->mode;
 	__u16 old_frag_off, new_frag_off;
 
-	/* make_writable might invoke copy-on-write, so fetch iph afterwards */
-	if (!skb_make_writable(skb, sizeof(struct iphdr))){
+	/* skb_ensure_writable might invoke copy-on-write, so fetch iph afterwards */
+	if (skb_ensure_writable(skb, sizeof(struct iphdr))){
 		printk(KERN_ERR "DF: Error making skb writable\n");
 		return NF_DROP;
 	}
